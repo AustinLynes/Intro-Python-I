0bfebe5d11cf519476f24cad85288a9f9d70b202
AustinLynes <austinlynes94@gmail.com>
2020-05-04 21:37:23 +0000

    been working been learning finished up to nubmer 5

--- src/03_modules.py
+++ src/03_modules.py
@@ -12,33 +12,40 @@ import sys
 # YOUR CODE HERE
 
 # find the length of the sys.argv property.
-count = len(sys.argv)
 # loop through the length
-for i in range(count):
-    print(f'{sys.argv[i]} \n')
+print(f'sys : #1')
+for i in range(len(sys.argv)):
+    print(f'argv property {i}: {sys.argv[i]} \n')
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
-os = sys.platform
-print(f'{os}')
+print(f'sys : #2')
+print(f'operating_system: {sys.platform} \n')
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
-py_version = sys.version[0]
-print(f'{py_version}')
-
+# for i in range(len(sys.version)):
+#     print(f'{sys.version[i]} \n')
+print(f'sys : #3')
+print(f'python_version: {sys.version}  \n')
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 # Print the current process ID
 # YOUR CODE HERE
-print(f'{os.getpgid(0)}')
+print(f'os : #1')
+print(f'current_process_id: {os.getpgid(0)} \n')
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-print(f'{os.getcwd()}')
+print(f'os : #2')
+print(f'current_working_directory: {os.getcwd()}')
 # Print out your machine's login name
 # YOUR CODE HERE
-print(f'{os.getlogin()}') # vagrant
+print(f'os : #3')
+print(f'login_username: {os.getlogin()}') # vagrant
+
+## my idea behind this stretch was to see if because since i setup the vagrant client 
+## and now an using the linux enviorment for programming so i was wondering if the method that 
+## the docs mentioned would get my username.. and. 
+print(' -- stretch --')
 import getpass
-print(f'{getpass.getuser()}') # still vagrant... oh yeah... vagrant is a VE {virtual enviorment... so UBUNTU is what this is ran in..  aka linux.. makes sense}
+print(f'login_username_with_getpass: {getpass.getuser()}') # still vagrant... oh yeah... vagrant is a VE {virtual enviorment... so UBUNTU is what this is ran in..  aka linux.. makes sense}
