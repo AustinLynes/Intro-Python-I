526a9be163ed846dbc46ec2489c8528af79acf74
AustinLynes <austinlynes94@gmail.com>
2020-05-05 05:32:42 +0000

    finished calander program

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,32 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+# open up the file stream
+with open("src/foo.txt", "r") as f:
+    # get the text we are working with
+    foo_txt = f.read()
+    # print the text we are looking at
+    print(foo_txt)
+
+f.closed
+# end of file read
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open("src/bar.txt", "w") as b:
+    b.write("something is better than nothing. \n")
+    b.write('the answer is, 42. \n')
+    b.write('beauty comes in many forms. and sometimes the evilest of wemon have the prettiest faces \n')
+
+b.closed
+
+with open("src/bar.txt", "r") as b_r:
+    bar_txt = b_r.read()
+    print(bar_txt)
+
+b_r.closed
--- src/14_cal.py
+++ src/14_cal.py
@@ -25,8 +25,60 @@ optional, as this is a common convention in documentation.
 This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
 print out a calendar for April in 2015, but if you omit either the year or both values, 
 it should use today’s date to get the month and year.
+
 """
 
+from os import system, name
 import sys
 import calendar
 from datetime import datetime
+
+# check_num 
+# takes _a number and _b number
+# and checks to see if a > b
+# if a > b then returns error 
+
+def check_num(_a, _b):
+  if _a > _b:
+    print("error")
+
+
+def main(*args):
+  now = datetime.now()
+  date = (str)(now)[:10]
+  txt_cal = calendar.TextCalendar(6)
+  # splitting above to create inp.. 
+  # this creates a list we need to go inside the first index of the list 
+  # and see if its currently is an empty string.. meaning the user just hit [ enter ]
+  _month = date[5:7]
+  _year = date[:4]
+
+  if len(args[0]) == 3: 
+    y = (int)(args[0][2])
+    m = (int)(args[0][1])
+    print(m > 0)
+    if(m <= 12 and m > 0):
+      _month = m 
+    else: 
+      print('Month Error, Month should be less than 12 and greater than 0')
+      return
+    if(y <= 5000 and y >= 1):
+      _year = y
+    else: 
+      print('Year Error, Month should be no less than 1 and no greater than 5000')
+      return
+  elif len(args[0]) == 2:
+    m = (int)(args[0][1])
+    if(m <= 12 and m > 0):
+      _month = m 
+    else: 
+       print('Month Error, Month should be less than 12 and greater than 0')
+       return
+  elif( len(args[0]) > 3):
+    print('Too Many Arguments. expects maximum of { Month,  Year }')
+    return 
+
+  
+  print(txt_cal.formatmonth(themonth=(int)(_month), theyear=(int)(_year)))
+
+main(sys.argv)
